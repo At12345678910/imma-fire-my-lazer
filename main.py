4f3b98fdb2ef8ad592c0e2f9dd9190b3a6996580
At12345678910 <88213496+At12345678910@users.noreply.github.com>
2021-08-02 10:58:17 -0700

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
-    info.changeScoreBy(755775)
-    EAAAAAAAAAAAD.setPosition(randint(2, 1), randint(2, 1))
-    info.startCountdown(10)
-})
-let EAAAAAAAAAAAD : Sprite = null
-scene.setBackgroundColor(6)
-let bwaaaw = sprites.create(img`
+def on_on_overlap(sprite, otherSprite):
+    info.change_score_by(755775)
+    EAAAAAAAAAAAD.set_position(randint(2, 1), randint(2, 1))
+    info.start_countdown(10)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap)
+
+EAAAAAAAAAAAD: Sprite = None
+scene.set_background_color(6)
+bwaaaw = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . 1 1 1 . 1 1 1 . . . . . . 
@@ -22,9 +23,10 @@ let bwaaaw = sprites.create(img`
             . . . 2 2 2 2 2 2 2 . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . .
-    `, SpriteKind.Player)
-controller.moveSprite(bwaaaw)
-EAAAAAAAAAAAD = sprites.create(img`
+    """),
+    SpriteKind.player)
+controller.move_sprite(bwaaaw)
+EAAAAAAAAAAAD = sprites.create(img("""
         . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -41,4 +43,5 @@ EAAAAAAAAAAAD = sprites.create(img`
             8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . .
-    `, SpriteKind.Food)
+    """),
+    SpriteKind.food)
